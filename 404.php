031da47d1098e7480403742543f292b6f0d509a9
Leonard Woo <leo_916@yahoo.com>
2021-09-12 10:58:14 +0800

    fix bug 20210912

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
-<main>
+<main class="container mx-auto">
   <div class="error-page">
-    <h2 class="post-title">404 - <?php '页面没找到'; ?></h2>
+    <h2 class="post-title">404 - <?php _e('页面没找到'); ?></h2>
   </div>
 </main>
 
